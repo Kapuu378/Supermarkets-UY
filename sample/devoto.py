20fe687d9a0970a2dbdf522f23bb8c543e53d153
Francisco Gibert <franciscojoin@gmail.com>
2025-12-20 18:10:27 -0300

    Added try and except for testing purposes, it will be later replaced with better log functionalities

--- sample/devoto.py
+++ sample/devoto.py
@@ -80,10 +80,15 @@ class Devoto():
                     if k in flat
                 }
                 # I know this is hardcoded and it's not good but it will change
-                data["CLUS_ID"] = id
-                data["DATE"] = date
-                data["SMK_NAME"] = 'Devoto'
-                data["PROD_UI"] = data["PROD_ID"] + "-" + data["SMK_NAME"]
+                try:
+                    data["CLUS_ID"] = id
+                    data["DATE"] = date
+                    data["SMK_NAME"] = 'Devoto'
+                    data["PROD_UI"] = data["PROD_ID"] + "-" + data["SMK_NAME"]
+                except KeyError as e:
+                    print(e.args)
+                    print(data)
+
 
                 self.container.append(data)
 
