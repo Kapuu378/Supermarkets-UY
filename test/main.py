db627834d2cf68a364f9abf6f9e7db06ad979eb5
Francisco Gibert <franciscojoin@gmail.com>
2025-12-21 16:37:14 -0300

    Added commiting on blocks

--- test/main.py
+++ test/main.py
@@ -10,8 +10,13 @@ devoto = Devoto(
     base_url="https://www.devoto.com.uy/api/catalog_system/pub/products/search?&_from=0&_to=49&fq=productClusterIds:"
 )
 
-devoto.fetch_products(limit_clusters=1)
-devoto.remove_duplicates(subset=['PROD_ID'])
-devoto.merge_orm_objects(session=db_session, table=Products)
-devoto.add_orm_objects(session=db_session, table=Prices, if_not_exists=True, match=['PROD_UI', 'DATE'])
+block = 10
+for index, cluster_id in enumerate(devoto.cluster_ids):
+    if index % block == 0:
+        devoto.remove_duplicates(subset=['PROD_ID'])
+        devoto.merge_orm_objects(session=db_session, table=Products)
+        devoto.add_orm_objects(session=db_session, table=Prices, if_not_exists=True, match=['PROD_UI', 'DATE'])
+        devoto.container = []
+        db_session.commit()
+    devoto.fetch_product(cluster_id=cluster_id)
 
